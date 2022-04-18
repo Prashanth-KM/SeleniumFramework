7946735fac937044b62c33abe4bfbef01deea266
PrashanthKM <prashanth.kumar@xebia.com>
2022-04-18 07:45:49 +0530

    added dashboard page test cases

---
+++ src/test/java/Pages/PageObjects/DashboardPageObjects.java
@@ -0,0 +1,10 @@
+package Pages.PageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashboardPageObjects {
+
+    @FindBy(id = "")
+    public WebElement dashboard;
+}
